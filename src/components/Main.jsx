7bc9a4463a8c1e503f2ee87bbcadecc26572321e
CodingDango <babyyodaman2.0@gmail.com>
2025-10-11 18:31:06 +0800

    progress on ui refactor

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,9 @@
 import { FaPlusCircle } from "react-icons/fa";
+import { FaSearch } from 'react-icons/fa';
+import { FaSliders } from 'react-icons/fa6';
+import { FaChevronDown } from 'react-icons/fa6';
 import { usePopUpContext } from "../context/PopUpContext";
+import { BsFilter } from 'react-icons/bs';
 import { useFlashcardsContext } from "../context/FlashcardsContext";
 import FlashcardList from "./FlashcardList";
 import AddCardForm from "./AddCardForm";
@@ -16,52 +20,79 @@ const Main = () => {
           <h1 className="2xs:flex-1 text-3xl font-medium">My Decks</h1>
           <p className="text-black-light">6 decks</p>
         </div>
-
-        <button
-          onClick={() => openPopUp("Add Flash Card", <AddCardForm addFlashcard={addFlashcard}/>)}
-          className="w-full 2xs:w-auto button button--primary"
-        >
-          <span className="flex gap-my-xs items-center font-medium">
-            <FaPlusCircle/>
-            Add new Deck
-          </span>
-        </button>
       </div>
 
       <section>
-        <ul className="flex gap-my-xs">
-          <li>
-            <button className="button button--primary w-[150px]">
+        <div className="flex flex-col gap-my-md">
+          <ul className="flex gap-my-xs">
+            <li>
+              <button className="button button--primary w-[150px]">
+                <div className="w-full flex justify-between items-center gap-my-xs font-normal">
+                  <span>All</span>
+                  <span className="px-3 bg-black-xs/30 rounded-md">6</span>
+                </div>
+              </button>
+            </li>
+            <li>
+              <button className="button bg-black-lg text-black-light w-[150px] border border-black-md">
               <div className="w-full flex justify-between items-center gap-my-xs font-normal">
-                <span>All</span>
-                <span className="px-3 bg-black-xs/30 rounded-md">6</span>
-              </div>
-            </button>
-          </li>
-          <li>
-            <button className="button bg-black-md text-black-light w-[150px]">
-            <div className="w-full flex justify-between items-center gap-my-xs font-normal">
-                <span>Favorites</span>
-                <span className="px-3 bg-black-xs/30 rounded-md">2</span>
-              </div>
-            </button>
-          </li>
-        </ul>
-      </section>
+                  <span>Favorites</span>
+                  <span className="px-3 bg-black-xs/30 rounded-md">2</span>
+                </div>
+              </button>
+            </li>
+          </ul>
 
-      <section>
-        <div className="grid grid-cols-[1fr_auto] gap-my-sm">
-          <input 
-            placeholder="Search for a deck by topic"
-            className="bg-black-md px-4 py-3 rounded-md"
-          />
+          <div className="grid grid-cols-2 gap-my-md">
+            <div className="flex items-center bg-black-lg border border-black-md px-4 py-3 rounded-md">
+              <div className="pr-4 border-r border-black-xs">
+                <FaSearch size={20} className="text-black-light"/>
+              </div>
+                
+              <input 
+                placeholder="Search for a deck by topic"
+                className="pl-4 flex-1 bg-transparent outline-none"
+              />
+            </div>
 
-          <div className="flex gap-my-sm">
-            <button className="button button--white">Filters</button>
-            <button className="button button--white">Time</button>
+            <div className="flex gap-my-md">
+              <button className="flex-1 button bg-black-lg border border-black-md">
+                <div className="w-full flex justify-between items-center">
+                  <div className="flex items-center">
+                    <span className="pr-my-sm border-r border-r-black-xs text-black-light">
+                      <FaSliders size={20}/>
+                    </span>
+                    <span className="pl-my-sm">Filters</span>
+                  </div>
+                  <FaChevronDown />
+                </div>
+              </button>
+              <button className="flex-1 button bg-black-lg border border-black-md">
+                <div className="w-full flex justify-between items-center">
+                  <div className="flex items-center">
+                    <span className="pr-my-sm border-r border-r-black-xs text-black-light">
+                      <BsFilter size={20}/>
+                    </span>
+                    <span className="pl-my-sm">Time</span>
+                  </div>
+                  <FaChevronDown />
+                </div>
+              </button>
+              
+              <button
+                onClick={() => openPopUp("Add Flash Card", <AddCardForm addFlashcard={addFlashcard}/>)}
+                className="flex-1 2xs:w-auto button button--primary"
+              >
+                <span className="flex gap-my-xs items-center">
+                  <FaPlusCircle/>
+                  Add new Deck
+                </span>
+              </button>
+            </div>
+
           </div>
         </div>
-      </section>
+      </section> 
 
       <FlashcardList flashcards={flashcards} />
     </main>
